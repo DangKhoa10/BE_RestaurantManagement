20efe00c925864df597e0751d9e428020829962f
trunghoai123 <trunghoaiitiuh@gmail.com>
2023-04-16 17:15:19 +0700

    add getAccountById

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const accountController = require('../../controllers/account.controller');
-const router = express.Router()
+const express = require("express");
+const accountController = require("../../controllers/account.controller");
+const router = express.Router();
 
-router.post('/signUp' , accountController.signUp)
-router.post('/signIn' , accountController.signIn)
-router.post('/getAccountCustomerByAccessToken' , accountController.getAccountCustomerByAccessToken)
-router.post('/verifyOtp' , accountController.verifyOtp)
-router.post('/sendOtp' , accountController.sendOtp)
+router.post("/signUp", accountController.signUp);
+router.post("/signIn", accountController.signIn);
+router.post(
+  "/getAccountCustomerByAccessToken",
+  accountController.getAccountCustomerByAccessToken
+);
+router.post("/verifyOtp", accountController.verifyOtp);
+router.post("/sendOtp", accountController.sendOtp);
+router.get("/getAccountById/:userId", accountController.getAccountById);
 
-module.exports = router
+module.exports = router;

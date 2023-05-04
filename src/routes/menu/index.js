2c40d86fff0f3e49362866a96aee75617f207fc2
trunghoai123 <trunghoaiitiuh@gmail.com>
2023-05-05 01:02:06 +0700

    merged from main

--- src/routes/menu/index.js
+++ src/routes/menu/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const menuController = require("../../controllers/menu.controller");
 const router = express.Router();
 
-router.post("/addMenu", menuController.addMenu)
+router.post("/addMenu", menuController.addMenu);
 router.get("/getAllMenu", menuController.getAllMenu);
 router.post("/getMenuByAll", menuController.getMenuByAll);
 router.post("/getMenuByTypeMenuId", menuController.getMenuByTypeMenuId);
